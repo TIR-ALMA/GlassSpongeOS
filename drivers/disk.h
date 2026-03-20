1d05b667698f7c754604c4bca815c82614da0c6b
TIR-ALMA <visotskayoa@mail.ru>
2026-03-20 16:04:05 +0700

    Update disk.h

--- drivers/disk.h
+++ drivers/disk.h
@@ -1,6 +1,12 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include "../types.h"
+
 void disk_init();
+uint8_t disk_read_sector(uint32_t lba, uint8_t *buffer);
+uint8_t disk_write_sector(uint32_t lba, const uint8_t *buffer);
+int disk_detect();
 
 #endif
+
