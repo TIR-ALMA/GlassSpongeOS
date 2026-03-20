c8058812e6994482d7de5a057f61203c83e33f13
TIR-ALMA <visotskayoa@mail.ru>
2026-03-20 16:11:52 +0700

    Update timer.c

--- drivers/timer.c
+++ drivers/timer.c
@@ -1,9 +1,43 @@
+// drivers/timer.c
 #include "timer.h"
+#include "lib/printf.h"
+#include "../sched.h"
+#include "../lib/stdarg.h"
+#include "../liquid_nn.h"
+
+// Счетчик тиков с момента запуска
+volatile unsigned long timer_ticks = 0;
 
 void timer_init() {
-    // Инициализация таймера
+    // Настройка PIT (Programmable Interval Timer)
+    // Частота системного таймера 100 Гц (каждые 10 мс)
+    unsigned int divisor = 1193180 / 100; // 1193180 Hz - частота PIT
+    
+    // Отправить команду настройки делителя
+    __asm__ volatile("outb %0, %1" : : "a"((char)(divisor & 0xFF)), "Nd"(0x40)); // LSB
+    __asm__ volatile("outb %0, %1" : : "a"((char)((divisor >> 8) & 0xFF)), "Nd"(0x40)); // MSB
 }
 
 void timer_handler() {
-    // Обработчик таймера
+    timer_ticks++;
+    
+    // Обновить количество тиков у текущего процесса
+    if(current_process) {
+        current_process->ticks_remaining--;
+        if(current_process->ticks_remaining <= 0) {
+            current_process->state = PROC_READY;
+            schedule();
+        }
+    }
+    
+    // Периодическое обучение нейросети
+    if(timer_ticks % 100 == 0) { // Каждую секунду
+        // Пример обучения: PID=1, время=0, частота=100, активен=1
+        kernel_liquid_predict(1, timer_ticks, 100, 1);
+    }
+    
+    // Отправить EOI (End of Interrupt) контроллеру прерываний
+    __asm__ volatile("outb $0x20, $0xA0"); // Slave PIC
+    __asm__ volatile("outb $0x20, $0x20"); // Master PIC
 }
+
