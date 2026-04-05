f50cf83b54f97a1304fa65e79fc1ba80359952bb
TIR-ALMA <visotskayoa@mail.ru>
2026-04-05 13:28:38 +0700

    Update string.c

--- lib/string.c
+++ lib/string.c
@@ -29,3 +29,23 @@ size_t strlen(const char *str) {
     while (str[len]) len++;
     return len;
 }
+
+uint16_t htons(uint16_t hostshort) {
+    return ((hostshort & 0xFF) << 8) | ((hostshort >> 8) & 0xFF);
+}
+
+uint32_t htonl(uint32_t hostlong) {
+    return ((hostlong & 0xFF) << 24) | 
+           (((hostlong >> 8) & 0xFF) << 16) | 
+           (((hostlong >> 16) & 0xFF) << 8) | 
+           ((hostlong >> 24) & 0xFF);
+}
+
+uint16_t ntohs(uint16_t netshort) {
+    return htons(netshort);
+}
+
+uint32_t ntohl(uint32_t netlong) {
+    return htonl(netlong);
+}
+
